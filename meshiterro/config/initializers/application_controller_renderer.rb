3010d43f12f7137915280995526adde13a1af29d
Yuichi-K-11 <banaichi1@gmail.com>
2020-11-18 13:56:59 +0000

    [start]app2

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '7e609f9d224c98969a8269ea7261788ad365145014c6c354753347a51e5877884a1514b12fcefe0395f51b963ebd5ede63f024b060903f9515797dae6e7fd5a6'
